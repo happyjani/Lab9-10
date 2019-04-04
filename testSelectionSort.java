e6f099b4deac1fac0d426b242b89ed3e00480d36
happyjani <47431158+happyjani@users.noreply.github.com>
2019-04-04 16:01:13 -0400

    Update testSelectionSort.java

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -21,6 +21,45 @@ public class testSelectionSort {
 		testMixed();
 	}
 }
+
+public void testPositive(){
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+
+        assertEquals(10,10);
+
+    }
+
+    public void testNegative(){
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -2;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -9;
+        Sortedarr[4] = -10;
+
+        assertEquals(-10,-10);
+
+    }
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
